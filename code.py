e799500b81c079fa407303a754aad974afd43b50
Theo Banjo <67348056+joshkin2@users.noreply.github.com>
2024-09-26 16:33:34 +0100

    Update code.py

--- code.py
+++ code.py
@@ -15,4 +15,13 @@ best_math_schools = best_math_schools.sort_value(by='average_math', ascending = 
 # create new column by summing math, reading, and writing scores
 schools['total_SAT']= schools['average_math'] + schools['average_reading'] + schools['average_writing']
 # sort df by "total_SAT" in descending order and select top 10 schools
-top_10
+top_10_schools = schools[['school_name', 'total_SAT']].sort_values(by= 'total_SAT',ascending= False).head(10)
+# STEP 3 - Locating NYC borough with largest SD in SAT performance
+# group data by borough and calculate no of schools, mean, and SD of total_SAT, also round to 2 decimal place
+borough_stats= schools.groupby('borough').agg(num_schools= ('school_name', 'count'), average_SAT = ('total_SAT', 'mean'), std_SAT= ('total_SAT','std')).round(2)
+# find borough with largest SD of total_SRT
+largest_std_dev = borough_stats[borough_stats['std_SAT'] == borough_stats['std_SAT'].max()]
+# reset index to include borough name in the DF
+largest_std_dev = largest_std_dev.reset_index()
+# display result
+print(largest_std_dev)
